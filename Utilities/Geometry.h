9d620c69c3b035f9f0fcfecc30d663f5abb186d3
rkevingibson <rkevingibson@gmail.com>
2016-08-25 10:15:29 -0400

    Added some geometry stuff, beginning to flesh out Mat3-4

--- Utilities/Geometry.h
+++ Utilities/Geometry.h
@@ -93,6 +93,14 @@ namespace rkg {
 		
 		inline float Length() const noexcept { return std::sqrt(x*x + y*y + z*z); }
 	
+		inline friend bool operator==(const Vec3& lhs, const Vec3& rhs) {
+			return lhs.x == rhs.x && lhs.y == rhs.y && lhs.z == rhs.z;
+		}
+
+		inline friend bool operator!=(const Vec3& lhs, const Vec3& rhs) {
+			return lhs.x != rhs.x || lhs.y != rhs.y || lhs.z != rhs.z;
+		}
+
 		//====================Compound Operators====================//
 
 		//Vector addition
@@ -158,7 +166,7 @@ namespace rkg {
 		}
 	
 		inline friend Vec3 Normalize(const Vec3& a) {
-			auto l = a.length();
+			auto l = a.Length();
 			return Vec3(a.x / l, a.y / l, a.z / l);
 		}
 	
@@ -233,7 +241,7 @@ namespace rkg {
 
 	struct Mat3
 	{
-		float data[9];
+		float data[9]{};
 
 		//=====================Access operators=====================
 		inline float& operator()(unsigned int row, unsigned int col)
@@ -319,6 +327,11 @@ namespace rkg {
 	{
 		float data[16];
 
+		inline void SetZero()
+		{
+			memset(data, 0, sizeof(data));
+		}
+
 		inline float& operator()(unsigned int row, unsigned int col)
 		{
 			return data[4 * col + row];
@@ -327,6 +340,43 @@ namespace rkg {
 		inline const float& operator()(unsigned int row, unsigned int col) const
 		{
 			return data[4 * col + row];
+		}
+
+		inline float& operator[](unsigned int idx)
+		{
+			return data[idx];
+		}
+
+		inline const float& operator[](unsigned int idx) const
+		{
+			return data[idx];
+		}
+
+
+		inline friend Mat4 operator*(const Mat4& lhs, const Mat4& rhs)
+		{
+			Mat4 x;
+			//Sure, this makes more sense than using a loop...
+			x[0] = lhs[0] * rhs[0] + lhs[4] * rhs[1] + lhs[8] * rhs[2] + lhs[12] * rhs[3];
+			x[1] = lhs[1] * rhs[0] + lhs[5] * rhs[1] + lhs[9] * rhs[2] + lhs[13] * rhs[3];
+			x[2] = lhs[2] * rhs[0] + lhs[6] * rhs[1] + lhs[10] * rhs[2] + lhs[14] * rhs[3];
+			x[3] = lhs[3] * rhs[0] + lhs[7] * rhs[1] + lhs[11] * rhs[2] + lhs[15] * rhs[3];
+			
+			x[4] = lhs[0] * rhs[4] + lhs[4] * rhs[5] + lhs[8] * rhs[6] + lhs[12] * rhs[7];
+			x[5] = lhs[1] * rhs[4] + lhs[5] * rhs[5] + lhs[9] * rhs[6] + lhs[13] * rhs[7];
+			x[6] = lhs[2] * rhs[4] + lhs[6] * rhs[5] + lhs[10] * rhs[6] + lhs[14] * rhs[7];
+			x[7] = lhs[3] * rhs[4] + lhs[7] * rhs[5] + lhs[11] * rhs[6] + lhs[15] * rhs[7];
+
+			x[8]  = lhs[0] * rhs[8] + lhs[4] * rhs[9] + lhs[8] * rhs[10] + lhs[12] * rhs[11];
+			x[9]  = lhs[1] * rhs[8] + lhs[5] * rhs[9] + lhs[9] * rhs[10] + lhs[13] * rhs[11];
+			x[10] = lhs[2] * rhs[8] + lhs[6] * rhs[9] + lhs[10] * rhs[10] + lhs[14] * rhs[11];
+			x[11] = lhs[3] * rhs[8] + lhs[7] * rhs[9] + lhs[11] * rhs[10] + lhs[15] * rhs[11];
+
+			x[12] = lhs[0] * rhs[12] + lhs[4] * rhs[13] + lhs[8] * rhs[14] + lhs[12] * rhs[15];
+			x[13] = lhs[1] * rhs[12] + lhs[5] * rhs[13] + lhs[9] * rhs[14] + lhs[13] * rhs[15];
+			x[14] = lhs[2] * rhs[12] + lhs[6] * rhs[13] + lhs[10] * rhs[14] + lhs[14] * rhs[15];
+			x[15] = lhs[3] * rhs[12] + lhs[7] * rhs[13] + lhs[11] * rhs[14] + lhs[15] * rhs[15];
+			return x;
 		}
 
 	};
