90e8552a4920917ce4e528f13019e772a70d10e1
rkevingibson <rkevingibson@gmail.com>
2017-05-05 09:42:08 -0400

    Changes to the ECS

--- ECS/Entities.cpp
+++ ECS/Entities.cpp
@@ -1,37 +1,11 @@
 #include "Entities.h"
 
-#include <atomic>
 
-namespace
-{
-using namespace rkg::ecs;
-ComponentContainer<Entity> entities;
 
-std::atomic_uint64_t next_id;
-}
 
 namespace rkg
 {
 namespace ecs
 {
-
-Entity* CreateEntity()
-{
-	EntityID id = next_id++;
-	Entity* result = entities.Create(id);
-	result->entity_id = id;
-	return result;
-}
-
-Entity* GetEntity(EntityID id)
-{
-	return entities.Get(id);
-}
-
-void DestroyEntity(EntityID id)
-{
-	entities.Remove(id);
-}
-
 }
 }
--- ECS/Entities.h
+++ ECS/Entities.h
@@ -3,7 +3,7 @@
 
 #include "Utilities/Utilities.h"
 #include "Utilities/HashIndex.h"
-
+#include "Utilities/Geometry.h"
 
 namespace rkg
 {
@@ -14,11 +14,11 @@ using EntityID = uint32_t;
 
 struct Entity
 {
-	EntityID entity_id;
+	EntityID id;
 
 	//TODO:
 	//Also include a transform, other common data.
-
+	Mat4 transform{ rkg::Mat4::Identity };
 };
 
 struct Component
@@ -26,15 +26,67 @@ struct Component
 	EntityID entity_id;
 };
 
-Entity* CreateEntity();
-Entity* GetEntity(EntityID id);
-void DestroyEntity(EntityID id);
+class Scene
+{
+private:
+	HashIndex hash_index_;
+	std::vector<Entity> entities_;
+	uint64_t next_id_;
+public:
+	inline Entity* CreateEntity()
+	{
+		EntityID id = next_id_++;
+		entities_.emplace_back();
+		hash_index_.Add(id, entities_.size() - 1);
+		entities_.back().id = id;
+		return &entities_.back();
+	}
+
+	inline Entity* GetEntity(EntityID id)
+	{
+		uint32_t num_entities = entities_.size();
+		for (auto i = hash_index_.First(id);
+			i != HashIndex::INVALID_INDEX && i < num_entities;
+			i = hash_index_.Next(i)) {
+			if (entities_[i].id == id) {
+				return &entities_[i];
+			}
+		}
+	}
+
+	inline void DestroyEntity(EntityID id)
+	{
+		uint32_t num_components = entities_.size();
+		for (auto i = hash_index_.First(id);
+			i != HashIndex::INVALID_INDEX && i < num_components;
+			i = hash_index_.Next(i)) {
+			if (entities_[i].id == id) {
+				//Remove this entry by swapping it with the last one in the data_ array.
+				entities_[i] = entities_.back();
+				//Need to update the appropriate slot in the hash_index.
+				//How to do this?
+				auto other_index = entities_.size() - 1;
+				auto other_id = entities_[i].id;
+				hash_index_.Remove(other_id, other_index);
+				hash_index_.Remove(id, i);
+				hash_index_.Add(other_id, i);
+				entities_.pop_back();
+
+				return;
+			}
+		}
+	}
+
+	//For using range-based for loops.
+	inline auto begin() { return entities_.begin(); }
+	inline auto end() { return entities_.end(); }
+};
 
 template<typename T>
 class ComponentContainer
 {
 private:
-	static_assert(std::is_base_of<Component, T>::value || std::is_base_of<Entity, T>::value, "Invalid type for container.");
+	static_assert(std::is_base_of<Component, T>::value, "Invalid type for container.");
 
 	HashIndex hash_index_;
 	std::vector<T> data_;
@@ -100,6 +152,9 @@ public:
 	{
 		return data_.end();
 	}
+
+	inline auto begin() { return data_.begin(); }
+	inline auto end() { return data_.end(); }
 private:
 
 };
