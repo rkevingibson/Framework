11a844096e6959d702a49bb3c6d6af09218e78b6
Kevin Gibson <rkevingibson@gmail.com>
2017-06-19 13:40:25 -0400

    Fixed the Ring buffer implementation, so now it actually, y'know, compiles

--- Utilities/RingBuffer.h
+++ Utilities/RingBuffer.h
@@ -19,17 +19,23 @@ private:
 	unsigned int write_{ 0 };
 
 	MemoryBlock block_;
-	element_type buffer_;
+	element_type* buffer_;
 	size_t capacity_;
-
+	size_t mask_;
 	allocator_type allocator_{};
 
 public:
 	RingBuffer(size_t capacity)
 	{
-		block_ = allocator_.Allocate(rkg::RoundToPow2(capacity));
+		block_ = allocator_.Allocate(rkg::RoundToPow2(capacity * sizeof(element_type)));
 		capacity_ = block_.length / sizeof(element_type);
-		buffer_ = static_cast<element_type>(block_.ptr);
+		mask_ = capacity_ - 1;
+		buffer_ = static_cast<element_type*>(block_.ptr);
+		//Need to initialize buffer_ objects to a valid state.
+		for (int i = 0; i < capacity_; i++) {
+			new(&buffer_[i]) element_type;
+		}
+
 	}
 
 	bool Push(const element_type& val)
@@ -37,24 +43,64 @@ public:
 		if (write_ == read_ + capacity_) {
 			return false;
 		}
-		buffer[write & (capacity_ - 1)] = val;
+		buffer[write & mask_] = val;
 		write_++;
 		return true;
 	}
 
 	bool Pop(element_type* val)
 	{
-		if (!val || read == write) {
+		if (!val || read_ == write_) {
 			return false;
 		}
-		*val = buffer[read & (capacity_ - 1)];
-		read++;
+		*val = buffer_[read_ & mask_];
+		read_++;
 		return true;
+	}
+
+	//Add an element to the buffer, overwriting the oldest element if the buffer is full. Will never fail.
+	bool PushOverwrite(const element_type& val)
+	{
+		if (write_ == read_ + capacity_) {
+			//Buffer is full, we're overwriting.
+			read_++;
+		}
+		buffer_[write_ & mask_] = val;
+		write_++;
+
+		return true;
+	}
+
+	//Access the elements as if it were a history - element 0 is the most recently pushed object.
+	element_type& operator[](size_t i)
+	{
+		Expects(i < capacity_);
+		return buffer_[(write_ - i - 1) & mask_];
+	}
+
+	const element_type& operator[](size_t i) const
+	{
+		Expects(i < capacity_);
+		return buffer[(write - i - 1) & mask_];
+	}
+
+	//How many elements are currently in the buffer
+	size_t Size() const
+	{
+		return (write_ - read_);
+	}
+
+	void Clear()
+	{
+		read_ =  0 ;
+		write_ = 0 ;
 	}
 
 	void Resize(size_t new_capacity)
 	{
 		allocator_.Reallocate(block_, RoundToPow2(new_capacity));
+		capacity_ = block_.length / sizeof(element_type);
+		mask_ = capacity_ - 1;
 	}
 };
 
