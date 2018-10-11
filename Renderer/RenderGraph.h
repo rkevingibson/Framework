1f55581e08f8db709d0e2fc2d0a750d4dba52bc0
Kevin Gibson <rkevingibson@gmail.com>
2018-10-10 21:23:02 -0700

    More rendergraph WIP

--- Renderer/RenderGraph.h
+++ Renderer/RenderGraph.h
@@ -1,12 +1,35 @@
 #pragma once
+#include <type_traits>
+#include <vector>
+
 
 namespace rkg {
 namespace render {
 
-struct RenderGraph {
+struct RenderGraphNode {
+	using ExecutionFn = void (*)(const RenderGraph& graph);
+	ExecutionFn                   fn;
+	RenderGraphNode*              parent;
+	std::vector<RenderGraphNode*> children;
 };
 
-struct RenderGraphNode {
+struct RenderGraph {
+	std::vector<RenderGraphNode> nodes;
 };
+
+struct RenderGraphFactory {
+	template <typename T, typename Compiler, typename Executor>
+	inline void AddNode(const char* name, Compiler&& compile_fn, Executor&& execute_fn){
+		static_assert(std::is_trivially_copyable_v<T>, "User data type must be trivially copyable");
+
+
+	};
+	RenderGraph* Compile();
+};
+
+struct ResourceManager {
+};
+
+
 }  // namespace render
 }  // namespace rkg
