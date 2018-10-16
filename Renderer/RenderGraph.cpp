38533b23b6daefd308d3877146825011e30963cd
Kevin Gibson <rkevingibson@gmail.com>
2018-10-15 22:42:07 -0700

    Basic dependency tracking is working

--- Renderer/RenderGraph.cpp
+++ Renderer/RenderGraph.cpp
@@ -4,18 +4,59 @@ namespace rkg {
 namespace render {
 RenderGraphResource ResourceManager::CreateBuffer(const BufferCreationInfo & info)
 {
+    RenderGraphResource handle;
+    handle.resource_index = resources.size();
+    handle.usage_id = 0;
+    resources.emplace_back();
+    auto& resource = resources.back();
+    resource.first_node_index = resource.last_node_index = current_node;
+    //TODO: Definitely need to store other stuff in the resource here.
 
-    return RenderGraphResource();
+    usages.emplace_back();
+    auto& usage = usages.back();
+    usage.usage = UsageEntry::Creation;
+    usage.resource_index = handle.resource_index;
+    usage.node_index = current_node;
+    return handle;
 }
 
-RenderGraphResource ResourceManager::ReadBuffer(RenderGraphResource)
+RenderGraphResource ResourceManager::ReadBuffer(RenderGraphResource handle)
 {
-    return RenderGraphResource();
+    //TODO: Need to handle case where read/write are in the same buffer. Should handle as if it was a call to UpdateBuffer.
+    Expects(handle.resource_index < resources.size());
+    handle.usage_id++;
+    
+    usages.emplace_back();
+    auto& usage = usages.back();
+    usage.usage = UsageEntry::Read;
+    usage.resource_index = handle.resource_index;
+    usage.node_index     = current_node;
+    return handle;
 }
 
-RenderGraphResource ResourceManager::WriteBuffer(RenderGraphResource)
+RenderGraphResource ResourceManager::UpdateBuffer(RenderGraphResource handle)
 {
-    return RenderGraphResource();
+    Expects(handle.resource_index < resources.size());
+    handle.usage_id++;
+    usages.emplace_back();
+    auto& usage = usages.back();
+    usage.usage = UsageEntry::ReadWrite;
+    usage.resource_index = handle.resource_index;
+    usage.node_index = current_node;
+    return handle;
+}
+
+RenderGraphResource ResourceManager::WriteBuffer(RenderGraphResource handle)
+{
+    Expects(handle.resource_index < resources.size());
+    handle.usage_id++;
+
+    usages.emplace_back();
+    auto& usage = usages.back();
+    usage.usage = UsageEntry::Write;
+    usage.resource_index = handle.resource_index;
+    usage.node_index = current_node;
+    return handle;
 }
 
 
--- Renderer/RenderGraph.h
+++ Renderer/RenderGraph.h
@@ -13,10 +13,15 @@ using LambdaAllocator =
     FallbackAllocator<CollectionOfStacksAllocator<Mallocator, KILO(2), 4>, Mallocator>;
 using UserDataAllocator = Mallocator;
 
-using RenderGraphResource = uint64_t;
+struct RenderGraph;
+struct RenderGraphFactory;
 
-struct BufferCreationInfo
-{
+struct RenderGraphResource {
+    uint32_t resource_index{0};
+    uint32_t usage_id{0};
+};
+
+struct BufferCreationInfo {
     enum UsageFlagBits {
         TransferSrcBit        = 0x01,
         TransferDstBit        = 0x02,
@@ -27,52 +32,51 @@ struct BufferCreationInfo
         IndexBufferBit        = 0x40,
         IndirectBufferBit     = 0x80,
     };
-    size_t size;
+    size_t   size;
     uint32_t usage;
-
 };
 
 struct ResourceManager {
-	RenderGraphResource CreateBuffer(const BufferCreationInfo& info);
+    RenderGraphResource CreateBuffer(const BufferCreationInfo& info);
     RenderGraphResource ReadBuffer(RenderGraphResource);
+    RenderGraphResource UpdateBuffer(RenderGraphResource);  // Read and Write in one pass.
     RenderGraphResource WriteBuffer(RenderGraphResource);
 
-    //Should not be called outside of the rendergraph. Maybe should be friend function?
-    inline void NewNode() {
-        current_node++;
-    }
-private:
-    struct Resource {
-        RenderGraphResource handle;
+    // Should not be called outside of the rendergraph. Maybe should be friend function?
 
+   private:
+    struct Resource {
+        size_t first_node_index;
+        size_t last_node_index;
     };
 
     struct UsageEntry {
         enum {
-            Read = 0x1,
-            Write = 0x2,
-            Creation = 0x4,
+            Read      = 0x1,
+            Write     = 0x2,
+            ReadWrite = 0x3,
+            Creation  = 0x3,  // Treat creation as a read/write for now.
         };
-        size_t node_index;
-        size_t resource_index;
-        uint8_t usage {0};
+        size_t  node_index;
+        size_t  resource_index;
+        uint8_t usage{0};
     };
-    std::vector<Resource> resources;
+    std::vector<Resource>   resources;
     std::vector<UsageEntry> usages;
 
-    size_t current_node {0};
+    size_t current_node{~0ull};
+    friend class RenderGraphFactory;
 };
-
-struct RenderGraph;
 
 struct RenderGraphNode {
     using ExecutionFn = void (*)(const ResourceManager&, void* execute_fn_buffer, void* user_data);
     ExecutionFn                   fn;
-    RenderGraphNode*              parent;
-    std::vector<RenderGraphNode*> children;
+    std::vector<RenderGraphNode*> parents;
     std::string                   name;
     MemoryBlock                   user_data_block;
     MemoryBlock                   executor_block;
+
+    friend class RenderGraphFactory;
 };
 
 struct RenderGraph {
@@ -83,24 +87,26 @@ struct RenderGraph {
 
     // TODO: Nit. Don't like this way of doing things. Would like to be able to execute the nodes
     // directly, via node->execute(). Preferably no arguments needed, though maybe passing the graph
-    // is a good idea. Alternatively, the nodes can contain a pointer to the graph, but that's somewhat circular and has risks with it.
+    // is a good idea. Alternatively, the nodes can contain a pointer to the graph, but that's
+    // somewhat circular and has risks with it.
 
     inline void ExecuteNode(RenderGraphNode* node)
     {
         node->fn(resource_manager, node->executor_block.ptr, node->user_data_block.ptr);
     };
+
+    friend class RenderGraphFactory;
 };
 
 
 
 struct RenderGraphFactory {
-    // TODO: More manual memory management once we're up and running.
-
+    static constexpr uint32_t    INVALID_NODE = ~0;
     std::unique_ptr<RenderGraph> graph{std::make_unique<RenderGraph>()};
     bool                         compiled{false};
 
     template <typename T, typename Setup, typename Executor>
-    inline void AddNode(const char* name, Setup&& setup_fn, Executor&& execute_fn)
+    inline const T& AddNode(const char* name, Setup&& setup_fn, Executor&& execute_fn)
     {
         static_assert(std::is_trivially_copyable_v<T>, "User data type must be trivially copyable");
         static_assert(
@@ -125,14 +131,52 @@ struct RenderGraphFactory {
         new (node.user_data_block.ptr) T;
 
         // Run Setup function
-        graph->resource_manager.NewNode();
+        graph->resource_manager.current_node++;
         setup_fn(graph->resource_manager, *reinterpret_cast<T*>(node.user_data_block.ptr));
+
+        return *reinterpret_cast<T*>(node.user_data_block.ptr);
     };
     inline std::unique_ptr<RenderGraph> Compile()
     {
+        Expects(compiled == false);
+
         // TODO: Figure out dependencies;
 
-        Expects(compiled == false);
+        struct ResourceTracking {
+            uint32_t last_write{INVALID_NODE};
+            uint32_t last_read{INVALID_NODE};
+        };
+
+        auto&                         nodes            = graph->nodes;
+        auto&                         resource_manager = graph->resource_manager;
+        std::vector<ResourceTracking> resource_tracking(resource_manager.resources.size());
+        for (size_t i = 0; i < resource_manager.resources.size(); ++i) {
+            resource_tracking[i].last_read  = resource_manager.resources[i].first_node_index;
+            resource_tracking[i].last_write = resource_manager.resources[i].first_node_index;
+        }
+
+        for (auto& usage : resource_manager.usages) {
+            // We have a dependency to track.
+
+            if (usage.usage & ResourceManager::UsageEntry::Read) {
+                // Add a dependency between this node and last written node.
+                ResourceTracking& tracking = resource_tracking[usage.resource_index];
+                if (tracking.last_write != usage.node_index) {
+                    nodes[usage.node_index].parents.push_back(&nodes[tracking.last_write]);
+                }
+
+                tracking.last_read = usage.node_index;
+            }
+
+            if (usage.usage & ResourceManager::UsageEntry::Write) {
+                ResourceTracking& tracking = resource_tracking[usage.resource_index];
+                if (tracking.last_read != usage.node_index) {
+                    nodes[usage.node_index].parents.push_back(&nodes[tracking.last_read]);
+                }
+                resource_tracking[usage.resource_index].last_write = usage.node_index;
+            }
+        }
+
         compiled = true;
         return std::move(graph);
     }
